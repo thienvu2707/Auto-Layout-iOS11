653d3dbe18f958411702563f59dcf3eed18e00bf
ThienVuLe <thienvurock@gmail.com>
2018-07-27 10:22:45 +0700

    Initial Commit

---
+++ auto_layout_swift4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  auto_layout_swift4
+//
+//  Created by Thien Vu Le on Jul/27/18.
+//  Copyright © 2018 Thien Vu Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
